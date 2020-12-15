17531c51768edbb6dfe285ff8d21eccb2e279ad7
Andrey Zentsov <welens@inbox.ru>
2020-12-15 15:43:33 +0300

    Add test for counter component

---
+++ src/app/playground/components/counter/counter.component.spec.ts
@@ -0,0 +1,31 @@
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+
+import {CounterComponent} from './counter.component';
+
+describe('CounterComponent', () => {
+  let component: CounterComponent;
+  let fixture: ComponentFixture<CounterComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [CounterComponent]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CounterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should inc for counter', () => {
+    component.increment()
+    expect(component.counter).toBe(11)
+  });
+
+  it('should decr for counter', () => {
+    component.decrement()
+    expect(component.counter).toBe(9)
+  });
+});
