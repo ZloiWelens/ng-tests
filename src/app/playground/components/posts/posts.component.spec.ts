34179a99393bc275dbd1c48146082c78a6c0f963
Andrey Zentsov <welens@inbox.ru>
2020-12-16 13:03:26 +0300

    Add check for user confirms or cancel

--- src/app/playground/components/posts/posts.component.spec.ts
+++ src/app/playground/components/posts/posts.component.spec.ts
@@ -37,6 +37,25 @@ describe('PostsComponent', () => {
     component.add('Post title')
 
     expect(component.message).toBe(err)
-  });
+  })
+
+  it('should remove post if user confirms', () => {
+    const spy = spyOn(service, 'remove').and.returnValue(EMPTY)
+    spyOn(window, 'confirm').and.returnValue(true)
+
+    component.delete(10)
+
+    expect(spy).toHaveBeenCalledWith(10)
+  })
+
+  it('should remove post if user cancel confirms', () => {
+    const spy = spyOn(service, 'remove').and.returnValue(EMPTY)
+    spyOn(window, 'confirm').and.returnValue(false)
+
+    component.delete(10)
+
+    expect(spy).not.toHaveBeenCalled()
+  })
+
 
 })
