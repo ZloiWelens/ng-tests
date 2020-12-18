87e9c939b32d90ef55e849d3c8b7812264eabef0
Andrey Zentsov <welens@inbox.ru>
2020-12-18 13:55:26 +0300

    DI test

--- src/app/playground/components/posts/posts.component.spec.ts
+++ src/app/playground/components/posts/posts.component.spec.ts
@@ -1,12 +1,32 @@
 import {PostsComponent} from './posts.component';
 import {PostsService} from './posts.service';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {HttpClientModule} from '@angular/common/http';
+import {of} from 'rxjs';
 
 describe('PostsComponent', () => {
+  let fixture: ComponentFixture<PostsComponent>
   let component: PostsComponent
   let service: PostsService
 
   beforeEach(() => {
-    service = new PostsService(null)
-    component = new PostsComponent(service)
+    TestBed.configureTestingModule({
+      declarations: [PostsComponent],
+      providers: [PostsService],
+      imports: [HttpClientModule]
+    })
+
+    fixture = TestBed.createComponent(PostsComponent)
+    component = fixture.componentInstance
+    service = TestBed.inject(PostsService)
   })
+
+  it('should fetch posts from ngOnInit', () => {
+    const posts = [1,2,3,4,5,6]
+    spyOn(service, 'fetch').and.returnValue(of(posts))
+
+    fixture.detectChanges()
+
+    expect(component.posts).toEqual(posts)
+  });
 })
